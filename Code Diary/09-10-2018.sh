1f79d616f394376e9f6e57bd56543bae146ae4f2
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-09-10 23:58:36 -0400

    Create 09-10-2018.sh

---
+++ Code Diary/09-10-2018.sh
@@ -0,0 +1,20 @@
+man
+Get information or help about a command
+apropos
+Locate commands by keyword look up
+ls
+List directory contents
+pwd
+Path of working directory
+cd
+Change working directory
+mv
+Move file or change name
+cp
+Copy file
+mkdir
+Make directory
+rm
+Remove files and directories
+mount /mnt/zip
+Mount zip disk on directory /mnt/zip
