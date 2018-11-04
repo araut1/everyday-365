22c2bb9f5cf6becc66da11666799b9c32307f36f
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-11-04 00:57:37 -0400

    multiplication table

--- Code Diary/07-31-2018.cpp
+++ Code Diary/07-31-2018.cpp
@@ -1,1 +1,15 @@
 //code
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int i = 1; i <= 10; i++)
+            System.out.print(N + " x " + i + " = " + N * i + "\n");
+        scanner.close();
+    }
+}
