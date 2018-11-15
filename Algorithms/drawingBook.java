0aadae067323e9951a13e6f6f79ea5e65f37a5d0
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-11-15 00:16:25 -0500

    Update drawingBook.java

--- Algorithms/drawingBook.java
+++ Algorithms/drawingBook.java
@@ -10,9 +10,44 @@ public class Solution {
      * Complete the pageCount function below.
      */
     static int pageCount(int n, int p) {
-        /*
-         * Write your code here.
-         */
+         int pCount = 0;
+        int leftTurns = -1;
+        int rightTurns = -1;
+
+        // left page turns
+        for (int i = 0; i <= n; i = i + 2) {
+
+            leftTurns++;
+            if ((i == p) || (i + 1 == p)) {
+                break;
+            }
+        }
+
+        // right page turns
+
+        if (n % 2 == 0) {
+
+            for (int i = n; i >= 0; i = i - 2) {
+
+                rightTurns++;
+                if ((i == p) || (i + 1 == p)) {
+                    break;
+                }
+
+            }
+        } else {
+
+            for (int i = n; i >= 1; i = i - 2) {
+
+                rightTurns++;
+                if ((i == p) || (i - 1 == p)) {
+                    break;
+                }
+
+            }
+        }
+        pCount = leftTurns <= rightTurns ? leftTurns : rightTurns;
+        return pCount;
 
     }
 
