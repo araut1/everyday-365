7e4b76456921588d7725d1e5bad338ea44aca52e
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-11-26 20:30:02 -0500

    Create serviceLane.java

---
+++ Algorithms/serviceLane.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] serviceLane(int n, int[] width, int[][] cases) {
+
+        int[] maxCarLength = new int[cases.length];
+        int entry, exit, min;
+
+        for (int i = 0; i < cases.length; i++) {
+            entry = cases[i][0];
+            exit = cases[i][1];
+            min = width[exit];
+            for (int j = entry; j < exit; j++) {
+                if (width[j] < min) {
+                    min = width[j];
+                }
+            }
+            maxCarLength[i] =min;
+        }
+
+
+        return maxCarLength;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nt = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nt[0]);
+
+        int t = Integer.parseInt(nt[1]);
+
+        int[] width = new int[n];
+
+        String[] widthItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int widthItem = Integer.parseInt(widthItems[i]);
+            width[i] = widthItem;
+        }
+
+        int[][] cases = new int[t][2];
+
+        for (int i = 0; i < t; i++) {
+            String[] casesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int casesItem = Integer.parseInt(casesRowItems[j]);
+                cases[i][j] = casesItem;
+            }
+        }
+
+        int[] result = serviceLane(n, width, cases);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
