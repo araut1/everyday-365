c96b487f9c7e99e58fc50e4fe19488d191ff98f6
Abhishek Dinkar Raut <9390583+araut1@users.noreply.github.com>
2018-11-27 19:10:40 -0500

    Create sequenceEquation.java

---
+++ Algorithms/sequenceEquation.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+
+
+        int l = p.length;
+        int[] x = new int[l];
+        int[] y = new int[l];
+        int[] mid = new int[l];
+
+        for (int i = 0; i < l; i++) {
+            x[i] = p[i];
+        }
+
+        Arrays.sort(x);
+
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+
+
+                if (x[i] == p[j]) {
+                    mid[i] = j+1;
+
+                }
+            }
+        }
+
+
+
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < l; j++) {
+
+
+                if (mid[i] == p[j]) {
+                    y[i] = j+1;
+
+                }
+            }
+        }
+
+
+
+
+        return y;
+
+
+
+
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
